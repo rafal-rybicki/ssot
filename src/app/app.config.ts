9d8a21db66f918214b282106f6484d7e251b9b82
Rafal <rafarybicki@gmail.com>
2024-02-21 17:09:05 +0100

    Add NgRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { projectsFeature } from './store/projects.feature';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideState(projectsFeature),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode()
+    })
+  ]
 };
