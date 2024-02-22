869a63c001e8502a573888c099e763e9432f9154
Rafal <rafarybicki@gmail.com>
2024-02-22 15:45:33 +0100

    Change project-page

--- src/app/pages/project-page/project-page.component.ts
+++ src/app/pages/project-page/project-page.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+
 import { TaskComponent } from '../../components/task/task.component';
+import { selectTasksState } from '../../store/tasks.feature';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-project-page',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [CommonModule, TaskComponent],
   templateUrl: './project-page.component.html',
   styleUrl: './project-page.component.scss',
   host: {
@@ -12,5 +19,21 @@ import { TaskComponent } from '../../components/task/task.component';
   }
 })
 export class ProjectPageComponent {
+  tasks: Task[] = [];
 
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.store.pipe(
+        select(selectTasksState),
+        map(
+          tasks => tasks.filter(task => task.projectId === params['id'])
+        )
+      ).subscribe(tasks => this.tasks = tasks)
+    })
+  }
 }
