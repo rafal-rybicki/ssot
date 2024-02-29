0c88f0db113e661aab2b63bb4d56b669fef5ccbe
Rafal <rafarybicki@gmail.com>
2024-02-29 19:53:41 +0100

    Add area component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,16 +4,19 @@ import { IconButtonComponent } from '../icon-button/icon-button.component';
 import { Store } from '@ngrx/store';
 import { selectActiveProjects } from '../../store/projects/projects.feature';
 import { CommonModule } from '@angular/common';
+import { AreaComponent } from './area/area.component';
+import { selectAreasState } from '../../store/areas/areas.feature';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, MenuItemComponent, IconButtonComponent],
+  imports: [CommonModule, MenuItemComponent, IconButtonComponent, AreaComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
   @HostBinding('class.open') get class() { return this.isOpen };
+  areas$ = this.store.select(selectAreasState);
   projects$ = this.store.select(selectActiveProjects);
   isOpen = true;
 
