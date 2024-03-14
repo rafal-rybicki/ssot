ccbc6e8272bcfc9cb54c6f74719039f0fce6f8af
Rafal <rafarybicki@gmail.com>
2024-03-14 17:55:40 +0100

    Add login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  host: {
+    '[class]': "'auth'",
+  }
+})
+export class LoginComponent {
+  private auth = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
+
+  msg = '';
+
+  loginForm = this.formBuilder.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmit() {
+    this.auth.login(
+      this.loginForm.controls.email.value,
+      this.loginForm.controls.password.value
+    ).subscribe(msg => this.msg = msg)
+  }
+}
