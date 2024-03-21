dc11865cfd067b3cf6e22865aced2e51f5841c23
Rafal <rafarybicki@gmail.com>
2024-03-21 14:13:23 +0100

    Move habits component

---
+++ src/app/features/habits/components/habit/habit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit',
+  standalone: true,
+  imports: [],
+  templateUrl: './habit.component.html',
+  styleUrl: './habit.component.scss'
+})
+export class HabitComponent {
+
+}
--- src/app/features/habits/habits.routes.ts
+++ src/app/features/habits/habits.routes.ts
@@ -3,7 +3,7 @@ import { authGuard } from '../../core/guards/auth.guard';
 import { HabitNewComponent } from './components/habit-new/habit-new.component';
 import { HabitEditComponent } from './components/habit-edit/habit-edit.component';
 import { HabitComponent } from './components/habit/habit.component';
-import { HabitsComponent } from './habits.component';
+import { HabitsComponent } from './components/habits/habits.component';
 
 export const habitRoutes: Routes = [
     { 
